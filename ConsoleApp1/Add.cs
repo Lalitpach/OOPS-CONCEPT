2824fe667aeecede2a0eb446026cffb8793e3805
Lalitpach <lalitpachpande111@gmail.com>
2024-10-22 18:26:05 +0800

    Add files via upload

---
+++ ConsoleApp1/Add.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Add
+    {
+        public int Add1( int a, int b)
+        {
+            return a + b;
+        }
+        public int Add1(int a, int b, int c)
+        {
+            return a + b + c;
+        }
+        public double Add1(double a,double b)
+        {
+            return a + b;
+        }
+
+    }
+}
---
+++ ConsoleApp1/Employee1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Employee1
+    {
+        public int id;
+        public string name;
+        public string Department;
+          
+        public Employee1()
+        {
+            id = 1;
+            name = "yashita";
+            Department = "it";
+        }
+        public Employee1(int id,string name)
+        {
+            id = 2;
+            name = "nita";
+            Department = "mech";
+        }
+        public Employee1(int id, string name,string Department)
+        {
+            id = 3;
+            name = "sita";
+            Department = "chem";
+
+        }
+        public void  Display()
+        {
+            Console.WriteLine($"id:{id} ,Name: {name},Department: {Department}");
+
+        }
+        
+    }
+}
